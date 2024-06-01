a7d055329acab4854644470ad9000b552ccef464
LucasSaller <sallerlucas@gmail.com>
2024-06-01 16:49:30 -0300

    add tooltip for add points

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,13 +12,16 @@ const Navbar = () => {
       </div>
       <div className="flex flex-row items-center gap-4">
         <h2 className="text-black">{user.name} </h2>
-        <div className="flex flex-row gap-1 bg-gray-300 py-2 px-3 rounded-2xl">
+        <div className="group flex flex-row gap-1 bg-gray-300 py-2 px-3 rounded-2xl relative">
           <button
             className="text-md text-black"
             onClick={() => addPoints(1000)}
           >
             {points}
           </button>
+          <span className="pointer-events-none group-hover:visible invisible absolute z-50 top-11 inline-block px-2 py-1 text-[10px] text-black font-medium tracking-tighter  bg-gray-300 rounded-lg shadow-sm">
+            Add points
+          </span>
           <Image src="/assets/coin.svg" alt="coin" width={20} height={20} />
         </div>
       </div>
