3f52f3c1f2e5b54f22ed375e7c1759fe94cee26a
LucasSaller <sallerlucas@gmail.com>
2024-06-01 16:05:21 -0300

    add Home Component

---
+++ app/components/Home.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Products from "@/app/components/Products/Products";
+import { Product } from "@/app/types/product";
+import { getProduts } from "../api/products";
+
+import Image from "next/image";
+import Loader from "./Loader";
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [status, setStatus] = useState<"pending" | "resolved" | "rejected">(
+    "pending"
+  );
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const products = await getProduts();
+        setProducts(products);
+        setStatus("resolved");
+      } catch (error) {
+        console.error("Error fetching products:", error);
+        setStatus("rejected");
+      }
+    };
+
+    fetchProducts();
+  }, []);
+  if (status == "pending") {
+    return <Loader />;
+  }
+  return (
+    <div className="w-full">
+      <div className="w-full h-[350px] p-4 relative ">
+        <Image
+          className="z-1"
+          src="/assets/header-x2.png"
+          alt="header"
+          fill
+          quality={100}
+          objectFit="cover"
+          objectPosition="center"
+        />
+        <h2 className="text-white text-5xl z-10 absolute bottom-10 left-40 tracking-wide font-bold">
+          Electronics
+        </h2>
+      </div>
+      {status === "resolved" && <Products products={products} />}
+    </div>
+  );
+};
+
+export default HomeScreen;
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,11 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
-import { getUser } from "./api/user";
-export default async function Home() {
-  const data = await getUser();
+import HomeScreen from "./components/Home";
 
+export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  bg-gray-100">
       <Navbar />
-      <h1 className="text-black">Hello Lucas</h1>
+      <HomeScreen />
     </main>
   );
 }
-// export async function getStaticProps() {
-//   // Call an external API endpoint to get posts.
-//   // You can use any data fetching library
-//   const res = await getUser();
-//   // const posts = await res.json();
-//   console.log(res);
-
-//   // By returning { props: { posts } }, the Blog component
-//   // will receive `posts` as a prop at build time
-//   return {
-//     props: {
-//       // posts,
-//     },
-//   };
-// }
