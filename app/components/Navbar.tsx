998484b844773e3f71779671f362fe752dd23142
LucasSaller <sallerlucas@gmail.com>
2024-06-03 14:08:23 -0300

    add tooltip for navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,16 @@
 import React, { useContext } from "react";
 import Image from "next/image";
 import { usePoints, useUser } from "../user/hooks";
+import { AnimatedTooltip } from "./ui/AnimatedTooltip";
 const Navbar = () => {
   const [points, addPoints] = usePoints();
   const user = useUser();
+  const item = {
+    cost: points,
+    name: "Add points",
+    designation: "1000",
+    image: "/assets/coin.svg",
+  };
   return (
     <div className="flex justify-between w-full py-5 px-10 shadow-lg fixed bg-white z-10">
       <div className="flex-1">
@@ -20,18 +27,15 @@ const Navbar = () => {
       </div>
       <div className="flex flex-row items-center gap-4">
         <h2 className="text-black">{user.name} </h2>
-        <div className="group bg-gray-300 py-2 px-3 rounded-2xl relative">
-          <button
+
+        <AnimatedTooltip item={item} onClick={() => addPoints(1000)} />
+        {/* <button
             onClick={() => addPoints(1000)}
             className="text-md text-black flex flex-row items-center gap-2"
           >
             {points}
             <Image src="/assets/coin.svg" alt="coin" width={20} height={20} />
-          </button>
-          <span className="pointer-events-none group-hover:visible invisible text-center w-[80px] absolute z-50 top-11 -left-3 block px-2 py-1 text-xs text-black font-light tracking-tighter  bg-gray-300 rounded-lg shadow-sm">
-            Add points
-          </span>
-        </div>
+          </button> */}
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/app/globals.css";
 import { UserProvider } from "./user/context";
 const inter = Inter({ subsets: ["latin"] });
 
